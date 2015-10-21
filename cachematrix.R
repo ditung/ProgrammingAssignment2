fd7ba18c33ca2ea3fee2b9a6442763c469d874e0
Dillon <dillontung@gmail.com>
2015-10-21 10:54:14 -0700

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
+##
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a list with an embedded matrix, that can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        m <- NULL
+        
+        ## If called, changes embedded matrix
+         set <- function(y) {
+                x <<- y
+                m <<- NULL
+         }
+        
+        ## Retrieves matrix that has been cached
+        get <- function() x
+        
+        ## Caches value of inverse
+        setinv <- function(inv) m <<- inv
+        
+        ## Recalls cached value of inverse
+        getinv <- function() m
+        
+        ## Creates list of functions with matrix embedded
+        list(get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x,' which is actually a list
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinv()
+        
+        ## Checks if inverse of matrix has been cached
+        ## If so, returns inverse and exits functions
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        
+        ## If not, solve for inverse, cache it and return it
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+
 }
+
+
+
+
